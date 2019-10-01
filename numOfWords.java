33324442bb97dde846a6f412901894dfede6b620
ishfulthinking <ishmaelkperez@gmail.com>
2019-10-01 17:16:15 -0400

    My second commit... I think I broke something :-/g

--- numOfWords.java
+++ numOfWords.java
@@ -7,15 +7,17 @@ public class numOfWords {
         System.out.println("Hey! Type a sentence and I'll count the number of words: ");
 
         String sentence = in.nextLine();
-        int numOfVowels = countVowels(sentence);
+        // Gee, I sure hope people understand what I mean by the below variable...
+        int variableName = countVowels(sentence);
         
-        System.out.println("There are " + numOfVowels + " vowels in that sentence.");
+        System.out.println("There are " + variableName + " vowels in that sentence.");
         in.close();
     }
 
     private static int countVowels(String str) {
         int vowelCount = 0;
 
+        // There has to be a better way to loop through this string...
         for (int index = 0; index < str.length(); index++) {
             char curr = str.charAt(index);
             if (isVowel(curr)) {
@@ -41,6 +43,7 @@ public class numOfWords {
             case 'y':
                 return true;
         }
+        // Wait, are we gonna catch every vowel? Seems sus to me...
 
         return false;
     }
